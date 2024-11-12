3361a57ac93c7db9b37296479852d5b76915972c
Florian CHAPUIS <fchapuis20@gmail.com>
2024-11-12 15:57:07 +0100

    Exercice terminé

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+10.times do
+  Article.create(title: Faker::Movie.title, content: Faker::Quote.matz)
+end
